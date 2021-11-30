9332cfdb1914286b1db56df7a895bfa2e544a8c2
Lautaro Romeo <lautiromeo337@gmail.com>
2021-11-30 11:51:33 -0300

    DB connection

--- ChristmasPresents/Model/ChristmasContext.cs
+++ ChristmasPresents/Model/ChristmasContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -26,7 +26,7 @@ namespace ChristmasPresents.Model
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySQL("server=localhost;user=root;password=password123;database=Christmas");
+                optionsBuilder.UseMySQL("server=sql449.main-hosting.eu;user=u990352211_navAdmin;password=ASDqwe123!@#;database=u990352211_navidad");
             }
         }
 
